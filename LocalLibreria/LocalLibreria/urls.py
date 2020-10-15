de974ca937b9fac978e57f972288660fafc06325
Nicolaskcd <nicolasskcc@gmail.com>
2020-10-15 13:08:06 -0300

    LocalLibreria: Creando Modelos

--- LocalLibreria/LocalLibreria/urls.py
+++ LocalLibreria/LocalLibreria/urls.py
@@ -1,4 +1,4 @@
-"""LocalLibreria URL Configuration
+"""locallibreria URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -13,8 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin 
-from django.urls import path 
+from django.contrib import admin
+from django.urls import path
 from django.urls import include
-urlpatterns = [ path ('admin/', admin.site.urls), 
-path ('catalogo/', include('catalogo.urls')), ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('catalogo/',include('catalogo.urls'))
+]
+
+urlpatterns+= static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
--- LocalLibreria/LocalLibreria/wsgi.py
+++ LocalLibreria/LocalLibreria/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for LocalLibreria project.
+WSGI config for locallibreria project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LocalLibreria.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locallibreria.settings')
 
 application = get_wsgi_application()
